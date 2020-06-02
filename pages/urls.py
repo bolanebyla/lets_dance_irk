0457f8b1537629ea0a8d31f671976531c241718b
bolanebyla <alexleskov2000@gmail.com>
2020-06-02 13:11:57 +0800

    Update urls.py

--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
-from django.conf import settings
-from django.conf.urls.static import static
+
 from . import views
 
 urlpatterns = [
@@ -18,5 +17,5 @@ urlpatterns = [
     path('', views.index, name='home'),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
